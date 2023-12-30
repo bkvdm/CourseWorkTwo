bdaf8a23fe1fdbfd642d2a95bd646f58645d1080
bkvdm <vadim@991199.ru>
2023-12-30 20:59:25 +0300

    Курсовая работа №2. Задание на курсовой проект: https://skyengpublic.notion.site/2-d001b0dcac29495f967a63673cf83f70

--- src/main/java/tel/bvm/courseWorkTwo/generator/Random.java
+++ src/main/java/tel/bvm/courseWorkTwo/generator/Random.java
@@ -1,4 +1,13 @@
 package tel.bvm.courseWorkTwo.generator;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Random {
+
+    public int getRandomGenerator(int maxValue) {
+        java.util.Random random = new java.util.Random();
+        int minValue = 1;
+        return random.nextInt((maxValue - minValue) + 1);
+    }
 }
