1ff784ed6c7b28f8e619f46d1e934720cce7815f
bkvdm <vadim@991199.ru>
2023-12-30 22:49:34 +0300

    Курсовая работа №2. Задание на курсовой проект: https://skyengpublic.notion.site/2-d001b0dcac29495f967a63673cf83f70

--- src/main/java/tel/bvm/courseWorkTwo/service/JavaQuestionService.java
+++ src/main/java/tel/bvm/courseWorkTwo/service/JavaQuestionService.java
@@ -1,9 +1,9 @@
 package tel.bvm.courseWorkTwo.service;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import tel.bvm.courseWorkTwo.generator.Random;
 import tel.bvm.courseWorkTwo.scheme.JavaQuestion;
+import tel.bvm.courseWorkTwo.scheme.Question;
 
 import java.util.Collection;
 
@@ -11,6 +11,26 @@ import java.util.Collection;
 public class JavaQuestionService extends QuestionServiceImpl{
     public JavaQuestionService(QuestionServiceImpl questionService) {
         super(questionService);
+    }
+
+    @Override
+    public JavaQuestion add(String question, String answer) {
+        return null;
+    }
+
+    @Override
+    public JavaQuestion remove(Question Question) {
+        return null;
+    }
+
+    @Override
+    public Collection<JavaQuestion> getAll() {
+        return null;
+    }
+
+    @Override
+    public Random getRandomQuestion(int maxValue) {
+        return null;
     }
 
 //public class JavaQuestionService implements QuestionService{
--- src/main/java/tel/bvm/courseWorkTwo/service/MathQuestionService.java
+++ src/main/java/tel/bvm/courseWorkTwo/service/MathQuestionService.java
@@ -2,6 +2,7 @@ package tel.bvm.courseWorkTwo.service;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import tel.bvm.courseWorkTwo.generator.Random;
 import tel.bvm.courseWorkTwo.scheme.JavaQuestion;
 import tel.bvm.courseWorkTwo.scheme.MathQuestion;
 import tel.bvm.courseWorkTwo.scheme.Question;
@@ -12,6 +13,26 @@ import java.util.Collection;
 public class MathQuestionService extends QuestionServiceImpl{
     public MathQuestionService(QuestionServiceImpl questionService) {
         super(questionService);
+    }
+
+    @Override
+    public MathQuestion add(String question, String answer) {
+        return null;
+    }
+
+    @Override
+    public MathQuestion remove(Question Question) {
+        return null;
+    }
+
+    @Override
+    public Collection<MathQuestion> getAll() {
+        return null;
+    }
+
+    @Override
+    public Random getRandomQuestion(int maxValue) {
+        return null;
     }
 //    private MathQuestionService QuestionService;
 //    public MathQuestionService(MathQuestionService questionService) {
