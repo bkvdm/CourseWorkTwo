4c44013484a01b1d661d84a60539070153be3dad
bkvdm <vadim@991199.ru>
2023-12-30 16:23:45 +0300

    Курсовая работа №2. Задание на курсовой проект: https://skyengpublic.notion.site/2-d001b0dcac29495f967a63673cf83f70

---
+++ src/main/java/tel/bvm/courseWorkTwo/CourseWorkTwoApplication.java
@@ -0,0 +1,13 @@
+package tel.bvm.courseWorkTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkTwoApplication.class, args);
+	}
+
+}
