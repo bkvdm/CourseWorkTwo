124421c55f124dcf329e5f0176a49f31dc889359
bkvdm <vadim@991199.ru>
2024-01-02 23:29:16 +0300

    Курсовая работа №2. Задание на курсовой проект: https://skyengpublic.notion.site/2-d001b0dcac29495f967a63673cf83f70

--- src/main/java/tel/bvm/courseworktwo/service/ExaminerServiceImpl.java
+++ src/main/java/tel/bvm/courseworktwo/service/ExaminerServiceImpl.java
@@ -15,13 +15,13 @@ public class ExaminerServiceImpl implements ExaminerService {
 
     private JavaQuestionService javaQuestionService;
     private MathQuestionService mathQuestionService;
-//    private ExaminerService examinerService;
+    private ExaminerService examinerService;
 
     @Autowired
     public ExaminerServiceImpl(JavaQuestionService javaQuestionService, MathQuestionService mathQuestionService) {
         this.javaQuestionService = javaQuestionService;
         this.mathQuestionService = mathQuestionService;
-//        this.examinerService = examinerService;
+        this.examinerService = examinerService;
     }
 
     @Override
@@ -45,86 +45,8 @@ public class ExaminerServiceImpl implements ExaminerService {
             return listExamQuestions;
         }
     }
-
-//    public Collection<Question> getQuestion(int amount) {
-//
-//        int possibleNumberExamQuestions = mathQuestionService.getAll().size() + javaQuestionService.getAll().size();
-//        List<Question> listExamQuestions = new ArrayList<>();
-//        Question questionOnly = new Question("", "Ответь на вопрос зкзамена");
-//
-//        if (amount > possibleNumberExamQuestions) {
-//            throw new QuestionsFull("Количество экзаменационных вопросов, больше их количества в списке");
-//        } else {
-//            while ((listExamQuestions.size() == amount)) {
-//                if (listExamQuestions.contains(javaQuestionService.getRandomQuestion())) {
-//                } else {
-//                    questionOnly.setQuestion(javaQuestionService.getRandomQuestion().getQuestion());
-//                    listExamQuestions.add(questionOnly);
-//                }
-//                if (listExamQuestions.contains(mathQuestionService.getRandomQuestion())) {
-//                } else {
-//                    questionOnly.setQuestion(mathQuestionService.getRandomQuestion().getQuestion());
-//                    listExamQuestions.add(mathQuestionService.getRandomQuestion());
-//                }
-//            }
-//            return listExamQuestions;
-//        }
-//    }
-
-//
-//        if (javaQuestionService.getAll().isEmpty() || mathQuestionService.getAll().isEmpty()) {
-//            throw new QuestionsNull("Список экзаменационных вопросов по одному из предметов не заполнен");
-//        } else {
-//            if (amount == javaQuestionService.getAll().size() || javaQuestionService.getAll().size() == 1) {
-//                listExamQuestions.addAll(javaQuestionService.getAll());
-//            } else {
-//                while (listExamQuestions.size() != amount) {
-//                    if (listExamQuestions.contains(javaQuestionService.getRandomQuestion())) {
-//                    } else {
-//                        listExamQuestions.add(javaQuestionService.getRandomQuestion());
-//                    }
-//                }
-//                ((amount - listExamQuestions.size()) == mathQuestionService.getAll().size() || mathQuestionService.getAll().size() == 1)
-//            }
-//
-//        }
-//
-//
-//
-//
-//
-//
-//        if (mathQuestionService.getAll().isEmpty()) {
-//            throw new QuestionsNull("Список экзаменационных вопросов по Математике пуст");
-//        } else {
-//            if (amount == mathQuestionService.getAll().size() || mathQuestionService.getAll().size() == 1) {
-//                return mathQuestionService.getAll();
-//            } else {
-//                while (listExamQuestions.size() != amount) {
-//                    if (listExamQuestions.contains(mathQuestionService.getRandomQuestion())) {
-//                    } else {
-//                        listExamQuestions.add(mathQuestionService.getRandomQuestion());
-//                    }
-//                }
-//            }
-//        }
-//        if (javaQuestionService.getAll().isEmpty()) {
-//            throw new QuestionsNull("Список экзаменационных вопросов по Java пуст");
-//        } else {
-//            if (amount == javaQuestionService.getAll().size() || javaQuestionService.getAll().size() == 1) {
-//                return javaQuestionService.getAll();
-//            } else {
-//                while (listExamQuestions.size() != amount) {
-//                    if (listExamQuestions.contains(javaQuestionService.getRandomQuestion())) {
-//                    } else {
-//                        listExamQuestions.add(javaQuestionService.getRandomQuestion());
-//                    }
-//                }
-//            }
-//        }
-        return listExamQuestions;
 }
-}
+
 //    public Collection<Question> getQuestions(int amount) {
 //        return mathQuestionService
 //                .getAll()
