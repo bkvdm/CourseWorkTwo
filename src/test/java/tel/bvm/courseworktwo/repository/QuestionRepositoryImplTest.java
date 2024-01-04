49dfbd2579ada7da7c9c11199e8077a318c52881
bkvdm <vadim@991199.ru>
2024-01-05 00:23:20 +0300

    Курсовая работа №2. Задание на курсовой проект: https://skyengpublic.notion.site/2-d001b0dcac29495f967a63673cf83f70

---
+++ src/test/java/tel/bvm/courseworktwo/repository/QuestionRepositoryImplTest.java
@@ -0,0 +1,29 @@
+package tel.bvm.courseworktwo.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionRepositoryImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
---
+++ src/test/java/tel/bvm/courseworktwo/service/ExaminerServiceImplTest.java
@@ -0,0 +1,17 @@
+package tel.bvm.courseworktwo.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExaminerServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getQuestion() {
+    }
+}
