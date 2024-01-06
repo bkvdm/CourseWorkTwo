e88b089e3deb883fa87bdced3478481318d7c8f6
bkvdm <vadim@991199.ru>
2024-01-06 16:36:21 +0300

    Курсовая работа №2. Задание на курсовой проект: https://skyengpublic.notion.site/2-d001b0dcac29495f967a63673cf83f70

--- src/test/java/tel/bvm/courseworktwo/service/ExaminerServiceImplTest.java
+++ src/test/java/tel/bvm/courseworktwo/service/ExaminerServiceImplTest.java
@@ -2,25 +2,58 @@ package tel.bvm.courseworktwo.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tel.bvm.courseworktwo.generator.RandomIndex;
+import tel.bvm.courseworktwo.repository.JavaQuestionRepositoryImpl;
+import tel.bvm.courseworktwo.repository.MathQuestionRepositoryImpl;
 import tel.bvm.courseworktwo.repository.QuestionRepositoryImpl;
+import tel.bvm.courseworktwo.scheme.Question;
+
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static tel.bvm.courseworktwo.constants.DefaultQuestionTest.*;
 
+@ExtendWith(MockitoExtension.class)
 class ExaminerServiceImplTest {
 
-    private QuestionRepositoryImpl questionRepository;
-
+    @Mock
     private QuestionServiceImpl questionService;
 
-    @BeforeEach
-    void setUp() {
-//        questionRepository = Mockito.mock(QuestionRepositoryImpl.class);
-//        questionService =
+    @Mock
+    private RandomIndex randomIndex;
 
+    @InjectMocks
+    private ExaminerServiceImpl examinerServiceOut;
+
+    @BeforeEach
+    void initOut() {
+        when(randomIndex.getRandomGenerator(COLLECTION_TEST.size()));
+        when(questionService.getAll()).thenReturn(COLLECTION_TEST);
     }
 
     @Test
     void getQuestion() {
+        Collection<Question> getQuestion = examinerServiceOut.getQuestion(1);
+        int actual = getQuestion.size();
+        assertEquals(actual, 1);
     }
 }
+
+//    private JavaQuestionRepositoryImpl javaQuestionRepository;
+//    private MathQuestionRepositoryImpl mathQuestionRepository;
+//
+//    private QuestionServiceImpl mathQuestionService;
+//    private QuestionServiceImpl javaQuestionService;
+//
+//    @BeforeEach
+//    void setUp() {
+//        questionRepository = Mockito.mock(QuestionRepositoryImpl.class);
+//        javaQuestionService = new QuestionServiceImpl(javaQuestionRepository);
+//
+//    }
