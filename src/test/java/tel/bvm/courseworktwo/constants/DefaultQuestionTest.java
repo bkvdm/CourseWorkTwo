8086fe7d970dfc6973e20faff8d2c48883a58931
bkvdm <vadim@991199.ru>
2024-01-06 18:56:33 +0300

    Курсовая работа №2. Задание на курсовой проект: https://skyengpublic.notion.site/2-d001b0dcac29495f967a63673cf83f70

--- src/test/java/tel/bvm/courseworktwo/constants/DefaultQuestionTest.java
+++ src/test/java/tel/bvm/courseworktwo/constants/DefaultQuestionTest.java
@@ -14,6 +14,13 @@ public class DefaultQuestionTest {
     public  static Question QUESTION_FIVE = new Question("Вопрос 5 (тест)", "Ответ 5 (тест)");
     public  static Question QUESTION_SIX = new Question("Вопрос 6 (тест)", "Ответ 6 (тест)");
 
+    public static Question QUESTION_ONLY_ONE = new Question("Вопрос 1 (тест)", "is hidden");
+    public  static Question QUESTION_ONLY_TWO = new Question("Вопрос 2 (тест)", "is hidden");
+    public  static Question QUESTION_ONLY_THREE = new Question("Вопрос 3 (тест)", "is hidden");
+    public  static Question QUESTION_ONLY_FOUR = new Question("Вопрос 4 (тест)", "is hidden");
+    public  static Question QUESTION_ONLY_FIVE = new Question("Вопрос 5 (тест)", "is hidden");
+    public  static Question QUESTION_ONLY_SIX = new Question("Вопрос 6 (тест)", "is hidden");
+
     public static Map<String, String> QUESTION_MAP_TEST = new HashMap<>(Map.of(
             QUESTION_ONE.getQuestion(), QUESTION_ONE.getAnswer(),
             QUESTION_TWO.getQuestion(), QUESTION_TWO.getAnswer(),
@@ -25,4 +32,21 @@ public class DefaultQuestionTest {
 
     public static List<Question> COLLECTION_TEST = new ArrayList<>(Arrays.asList(
             QUESTION_ONE, QUESTION_TWO, QUESTION_THREE, QUESTION_FOUR, QUESTION_FIVE, QUESTION_SIX));
+
+    public static List<Question> COLLECTION_TEST_ONE_AMOUNT = new ArrayList<>(Arrays.asList(
+            QUESTION_ONLY_ONE));
+
+    public static List<Question> COLLECTION_TEST_TWO_AMOUNT = new ArrayList<>(Arrays.asList(
+            QUESTION_ONLY_ONE, QUESTION_ONLY_TWO));
+
+    public static List<Question> COLLECTION_TEST_TREE_AMOUNT = new ArrayList<>(Arrays.asList(
+            QUESTION_ONLY_ONE, QUESTION_ONLY_TWO, QUESTION_ONLY_THREE));
+
+    public static List<Question> COLLECTION_TEST_FOUR_AMOUNT = new ArrayList<>(Arrays.asList(
+            QUESTION_ONLY_ONE, QUESTION_ONLY_TWO, QUESTION_ONLY_THREE, QUESTION_ONLY_FOUR));
+
+    public static List<Question> COLLECTION_TEST_FIVE_AMOUNT = new ArrayList<>(Arrays.asList(
+            QUESTION_ONLY_ONE, QUESTION_ONLY_TWO,QUESTION_ONLY_THREE, QUESTION_ONLY_FOUR, QUESTION_ONLY_FIVE));
+    public static List<Question> COLLECTION_TEST_SIX_AMOUNT = new ArrayList<>(Arrays.asList(
+            QUESTION_ONLY_ONE, QUESTION_ONLY_TWO, QUESTION_ONLY_THREE, QUESTION_ONLY_FOUR, QUESTION_ONLY_FIVE, QUESTION_ONLY_SIX));
 }
--- src/test/java/tel/bvm/courseworktwo/service/ExaminerServiceImplTest.java
+++ src/test/java/tel/bvm/courseworktwo/service/ExaminerServiceImplTest.java
@@ -3,6 +3,9 @@ package tel.bvm.courseworktwo.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -14,6 +17,7 @@ import tel.bvm.courseworktwo.repository.QuestionRepositoryImpl;
 import tel.bvm.courseworktwo.scheme.Question;
 
 import java.util.Collection;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -25,25 +29,53 @@ class ExaminerServiceImplTest {
     @Mock
     private QuestionServiceImpl questionService;
 
-    @Mock
-    private RandomIndex randomIndex;
+    RandomIndex randomIndex = new RandomIndex();
 
     @InjectMocks
     private ExaminerServiceImpl examinerServiceOut;
 
+
     @BeforeEach
     void initOut() {
-        when(randomIndex.getRandomGenerator(COLLECTION_TEST.size()));
         when(questionService.getAll()).thenReturn(COLLECTION_TEST);
+        when(questionService.getRandomQuestion()).thenReturn(COLLECTION_TEST.get(randomIndex.getRandomGenerator(COLLECTION_TEST.size() - 1)));
     }
 
-    @Test
-    void getQuestion() {
-        Collection<Question> getQuestion = examinerServiceOut.getQuestion(1);
-        int actual = getQuestion.size();
-        assertEquals(actual, 1);
+    public static Stream<Arguments> amountQuestionVariations() {
+        return Stream.of(Arguments.of(1, COLLECTION_TEST_ONE_AMOUNT),
+                Arguments.of(2, COLLECTION_TEST_TWO_AMOUNT),
+                Arguments.of(3, COLLECTION_TEST_TREE_AMOUNT),
+                Arguments.of(4, COLLECTION_TEST_FOUR_AMOUNT),
+                Arguments.of(5, COLLECTION_TEST_FIVE_AMOUNT),
+                Arguments.of(6, COLLECTION_TEST_SIX_AMOUNT)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("amountQuestionVariations")
+    void getQuestion(int amount, Collection <Question> expected) {
+        assertEquals(expected, examinerServiceOut.getQuestion(amount));
     }
 }
+
+//    @Test
+//    void getQuestion() {
+//        int actual = examinerServiceOut.getQuestion(1).size();
+//        assertEquals(actual, 1);
+//    }
+
+//        int actual = examinerServiceOut.getQuestion(1).size();
+//    @ParameterizedTest
+//    @MethodSource("positivePlusVariations")
+//    void plusPositiveMethod(Float one, Float two, String expected) {
+//        assertEquals(expected, calculatorService.plus(one, two));
+//    }
+
+//        when(randomIndex.getRandomGenerator(COLLECTION_TEST.size())).thenReturn(1);
+//        when(questionService.getRandomQuestion()).thenReturn(COLLECTION_TEST.get(1));
+
+//        Collection<Question> getQuestion = examinerServiceOut.getQuestion(1);
+//        int actual = getQuestion.size();
 
 //    private JavaQuestionRepositoryImpl javaQuestionRepository;
 //    private MathQuestionRepositoryImpl mathQuestionRepository;
